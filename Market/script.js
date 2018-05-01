265a8c815f2da1187601df78b860062b6aa0a503
Darya Ulyanova <dashaorluk@gmail.com>
2018-05-01 23:12:21 +0300

    deleted spaces

--- Market/script.js
+++ Market/script.js
@@ -1,6 +1,3 @@
-/**You need to find, weigh and buy 15 items in supermarket.
-Every item should have place, price and weight.
-Also, you need to get a bill for each item. */
 var Products = function (name, weight, price, place) {
     this.name = name;
     this.weight = weight;
@@ -10,7 +7,6 @@ var Products = function (name, weight, price, place) {
 
 Products.prototype.result = function () {
        return this.name + ' costed ' + this.weight * this.price +  ' grn and bought in the ' + this.place + ' department.';
-
 };
 var bananas = new Products ('Bananas', 3, 15, 'Fruit');
 console.log(bananas.result());
